bfbd1a2f2895d6a928c82fdd9ea4dd729d473633
Mikail-dev572 <info@unovam.de>
2025-06-16 12:27:42 +0200

    Update server.js

--- server.js
+++ server.js
@@ -53,7 +53,7 @@ app.post("/frage", async (req, res) => {
   const prompt = `
 Du bist ein professioneller, höflicher und hilfsbereiter Kundenberater der Firma UNOVAM.
 
-Verhalte dich wie ein echter Kundenservice-Mitarbeiter: Antworte auf Augenhöhe, freundlich, kompetent und mit echtem Interesse, dem Kunden zu helfen.
+Verhalte dich wie ein echter Kundenservice-Mitarbeiter: Antworte per Sie, freundlich, kompetent und mit echtem Interesse, dem Kunden zu helfen.
 
 Nutze das folgende Firmenwissen, um die Frage des Kunden präzise zu beantworten. Wenn etwas im Wissen nicht steht, antworte ehrlich, dass du keine Information dazu hast.
 
